8a22992111ec5229ecb39bb4869a6a50b5ba61f2
Piryus <piryus@gmail.com>
2018-06-03 14:31:10 +0200

    Add MainMenu documentation

--- view/MainMenu.cpp
+++ view/MainMenu.cpp
@@ -1,16 +1,28 @@
 #include "MainMenu.h"
 
+/**
+ * Default constructor
+ * The main menu is the first thing appearing at the screen, it has to create a window to be displayed in
+ */
 MainMenu::MainMenu() {
+    // Create window rendering settings
     sf::ContextSettings settings;
+    // Sets the antialiasing level to 8 to smooth nicely the shapes, buttons, ...
     settings.antialiasingLevel = 8;
+    // Creates a new window 1200*800 with the previously defined settings
     main_window_->create(sf::VideoMode(1200, 800), "Teeko", sf::Style::Default, settings);
+    // Sets the window's icon
     sf::Image icon;
-    icon.loadFromFile("..\\resources\\images\\icon.png");
+    icon.loadFromFile(R"(..\resources\images\icon.png)");
     main_window_->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 }
 
+/**
+ * Renders the main menu
+ * Contains the main loop of the menu and will call appropriate functions on click
+ */
 void MainMenu::Render() {
-    sf::Event event;
+    sf::Event event{};
     while (main_window_->isOpen()) {
         while (main_window_->pollEvent(event)) {
             switch (event.type) {
@@ -18,7 +30,7 @@ void MainMenu::Render() {
                     main_window_->close();
                     break;
                 }
-                case sf::Event::Resized: {  // Occur when window is resized
+                case sf::Event::Resized: {  // Occurs when window is resized
                     main_window_->setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
                     break;
                 }
@@ -30,40 +42,51 @@ void MainMenu::Render() {
             }
         }
 
+        // Draws the menu's elements
         this->DrawBackground();
         this->DrawTitle();
         this->DrawButtons();
-        // Display window
+        // Displays the window
         main_window_->display();
     }
 }
 
+/**
+ * Loads the "Teeko" logo image and displays it at the top of the menu
+ */
 void MainMenu::DrawTitle() {
+    // Loads the logo
     sf::Texture teeko_img;
-    teeko_img.loadFromFile("..\\resources\\images\\teeko.png");
+    teeko_img.loadFromFile(R"(..\resources\images\teeko.png)");
+    // Creates a sprite fitting the logo
     sf::Sprite teeko;
     teeko.setTexture(teeko_img);
-    teeko.setPosition((main_window_->getSize().x-teeko.getLocalBounds().width)/2,(main_window_->getSize().y-teeko.getLocalBounds().height)/12);
+    // The logo's position depends of the width and height of the window
+    teeko.setPosition((main_window_->getSize().x - teeko.getLocalBounds().width) / 2,
+                      (main_window_->getSize().y - teeko.getLocalBounds().height) / 12);
     main_window_->draw(teeko);
 }
 
-void MainMenu::DrawButtons() { //TODO(Piryus) Refactor this (soon)
-    // Buttons position
+/**
+ * Creates and displays the menu buttons and their texts
+ */
+void MainMenu::DrawButtons() {
+    // Buttons position, first button is at the center of the window
     int button_x = (main_window_->getSize().x - kButtonWidth) / 2;
     int button_y = (main_window_->getSize().y - kButtonHeight) / 2;
 
     // Create play button
-    play_two_button_ = CreateButton(button_x,button_y,sf::Color(230, 126, 34),sf::Color(211, 84, 0));
+    play_two_button_ = CreateButton(button_x, button_y, sf::Color(230, 126, 34), sf::Color(211, 84, 0));
     main_window_->draw(play_two_button_);
 
-    // Create "Play against AI" button
-    button_y = (main_window_->getSize().y - kButtonHeight) / 2 + kButtonHeight + 50;
-    play_ai_button_ = CreateButton(button_x,button_y,sf::Color(230, 126, 34),sf::Color(211, 84, 0));
+    // Create "Play against AI" button, 50px under the previous one
+    button_y += kButtonHeight + 50;
+    play_ai_button_ = CreateButton(button_x, button_y, sf::Color(230, 126, 34), sf::Color(211, 84, 0));
     main_window_->draw(play_ai_button_);
 
-    // Create "Exit" button
-    button_y = (main_window_->getSize().y - kButtonHeight) / 2 + kButtonHeight * 2 + 50 * 2;
-    exit_button_ = CreateButton(button_x,button_y,sf::Color(230, 126, 34),sf::Color(211, 84, 0));
+    // Create "Exit" button, 50px under the previous one
+    button_y += kButtonHeight + 50;
+    exit_button_ = CreateButton(button_x, button_y, sf::Color(230, 126, 34), sf::Color(211, 84, 0));
     main_window_->draw(exit_button_);
 
     // If mouse is on a button, change its color
@@ -76,22 +99,40 @@ void MainMenu::DrawButtons() { //TODO(Piryus) Refactor this (soon)
     main_window_->draw(CreateButtonText(&exit_button_, "Exit game"));
 }
 
+/**
+ * Checks whether the mouse pointer is on a button
+ * If it is, the function changes the button color
+ * @param mouse_x, the mouse position on X axis
+ * @param mouse_y, the mouse position on Y axis
+ */
 void MainMenu::HighlightButtonsController(int mouse_x, int mouse_y) {
+    // Creates a vector with the menu's buttons
     std::vector<sf::RectangleShape> buttons_vec{play_two_button_, play_ai_button_, exit_button_};
     for (sf::RectangleShape button : buttons_vec) {
+        // Checks if mouse pointer is on the button
         if (mouse_x >= button.getPosition().x &&
             mouse_x <= (button.getPosition().x + button.getSize().x) &&
             mouse_y >= button.getPosition().y &&
             mouse_y <= (button.getPosition().y + button.getSize().y)) {
+            // Changes its color
             button.setFillColor(sf::Color(39, 174, 96));
             main_window_->draw(button);
         }
     }
 }
 
+/**
+ * Manages events on click
+ * Checks whether the player clicked on a button or not
+ * If he did, executes the action linked to that button
+ * @param mouse_x, the mouse position on the X axis on click
+ * @param mouse_y, the mouse position on the Y axis on click
+ */
 void MainMenu::ClickController(int mouse_x, int mouse_y) {
+    // Creates a vector with the menu's buttons
     std::vector<sf::RectangleShape> buttons_vec{play_two_button_, play_ai_button_, exit_button_};
-    for (int i = 0; i < 3; i++) {
+    // Here we use the int i to iterate through the vector because buttons don't have IDs and we need to identify which button was clicked
+    for (int i = 0; i < buttons_vec.size(); i++) {
         if (mouse_x >= buttons_vec.at(i).getPosition().x &&
             mouse_x <= (buttons_vec.at(i).getPosition().x + buttons_vec.at(i).getSize().x) &&
             mouse_y >= buttons_vec.at(i).getPosition().y &&
@@ -111,6 +152,8 @@ void MainMenu::ClickController(int mouse_x, int mouse_y) {
                     main_window_->close();
                     break;
                 }
+                default:
+                    break;
             }
         }
     }
--- view/MainMenu.h
+++ view/MainMenu.h
@@ -3,19 +3,29 @@
 
 #include "AIMenu.h"
 
+/**
+ * @brief Class for creating and rendering a main menu
+ */
 class MainMenu : public Menu {
 private:
-    const int kBorderX = 750;
-    const int kBorderY = 750;
+    // The "Play (2 Players)" button of the main menu
     sf::RectangleShape play_two_button_;
+    // The "Play against A.I." button
     sf::RectangleShape play_ai_button_;
+    // The "Exit Game" button
     sf::RectangleShape exit_button_;
 public:
+    // Default constructor
     MainMenu();
+    // Calls the functions necessary to draw the background, the logo and the buttons, also wait for events (clicks)
     void Render() override;
+    // Draws the 3 buttons of the main menu
     void DrawButtons() override;
+    // On mouseover, changes the color of a button
     void HighlightButtonsController(int mouse_x, int mouse_y) override;
+    // Manages every events triggered by a mouse click
     void ClickController(int mouse_x, int mouse_y) override;
+    // Draws the big "Teeko" logo
     void DrawTitle();
 };
 
