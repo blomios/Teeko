76d7e9d38a2f6368d8ae190d0b49e7c5f0772290
Piryus <piryus@gmail.com>
2018-06-09 19:01:10 +0200

    Add SoundManager's documentation

--- view/SoundManager.h
+++ view/SoundManager.h
@@ -3,19 +3,28 @@
 
 #include "SFML/Audio.hpp"
 
+/**
+ * Class to manage sounds in the game
+ */
 class SoundManager {
 public:
+    // Default constructor
     SoundManager();
+    // Plays the sound of a marker being placed
     void PlayMarkerSound();
+    // Plays the victory music when game is over
     void PlayVictoryMusic();
+    // Plays the sound on button mouseover
     void PlayButtonClickSound();
 private:
+    // Buffers and sounds
     sf::SoundBuffer marker_sound_buffer_;
     sf::Sound marker_sound_;
     sf::SoundBuffer button_click_sound_buffer_;
     sf::Sound button_click_sound_;
     sf::Sound victory_music_;
     sf::SoundBuffer victory_music_buffer_;
+    // Boolean value used to block victory music to play twice
     bool victory_sound_played_;
 };
 
