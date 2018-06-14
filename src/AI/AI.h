7ede55eba7f820063822c0a1e130175007edbcfa
Piryus <piryus@gmail.com>
2018-06-14 15:40:05 +0200

    Reformat code in AI.h

--- src/AI/AI.h
+++ src/AI/AI.h
@@ -11,19 +11,32 @@ private:
 public:
     // AI's constructor
     AI(int difficulty, std::string color);
+
     // The main algorithm determining a board's evaluation to a certain depth
     int Minimax(vector<Space> board, int depth, bool is_maximizing, int alpha, int beta);
+
     // Evaluates the board and returns a score
     int Evaluate(vector<Space> *board);
+
+    // Evaluation function for the easy AI
     int EvaluateEasy(vector<int> red, vector<int> black);
+
+    // Evaluation function for the normal AI
     int EvaluateNormal(vector<int> red, vector<int> black);
+
+    // Evaluation function for the hard AI
     int EvaluateHard(vector<int> red, vector<int> black);
+
     int EvaluatePotential(vector<int> markers, int player);
+
     // Finds the best best move possible
     vector<int> FindBestMoveSpacesId(vector<Space> board);
+
     // Finds the best space to place a marker
     int FindBestPlacementSpaceId(vector<Space> board);
+
     int MarkerAlignment(vector<int> markers_id, int coef, int player);
+
     int MarkerDistance(vector<int> space_id, int coef, int player);
 };
 
